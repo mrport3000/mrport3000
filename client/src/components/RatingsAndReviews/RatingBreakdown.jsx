ff89487d8a30fbf539d312860a3eaf32e43344d2
Eric <e.kalin12@gmail.com>
2022-08-23 00:27:29 -0400

    refactoring code and working on logic to handle sort feature

--- client/src/components/RatingsAndReviews/RatingBreakdown.jsx
+++ client/src/components/RatingsAndReviews/RatingBreakdown.jsx
@@ -4,7 +4,7 @@ import React from 'react';
 import './RatingAndReview.css';
 
 function RatingBreakdown(props) {
-  console.log('props: ', props);
+  //console.log('props: ', props);
   return (
     <div className="eric-RR-productRatingContainer">
       {/* RatingBreakdown Placeholder! */}
--- client/src/components/RatingsAndReviews/SortReviews.jsx
+++ client/src/components/RatingsAndReviews/SortReviews.jsx
@@ -7,21 +7,36 @@ class SortReviews extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
+      totalReviews: [...this.props.reviews],
       sortTerm: '',
       sortedReviews: [],
     };
+    this.flowControl = this.flowControl.bind(this);
     this.handleChange = this.handleChange.bind(this);
   }
 
-  componentDidUpdate(e) {
-    console.log('updated event: ', e);
-  }
+  // componentDidUpdate(prevProps, prevState, ss) {
+  //   console.log('prevProps: ', prevProps);
+  //   console.log('prevState: ', prevState);
+  //   console.log('snapshot: ', ss);
+  // }
 
   handleChange(e) {
     this.setState({ sortTerm: e.target.value }, () => {
       // eslint-disable-next-line react/destructuring-assignment
-      console.log('sortTerm: ', this.state.sortTerm);
+      console.log('sortTerm: ', this.state);
+      this.flowControl(this.state.sortTerm);
     });
+  }
+
+  flowControl(term) {
+    if (term === 'relevent') {
+      console.log('relevent target matched');
+    } else if (term === 'newest') {
+      console.log('newest target matched');
+    } else if (term === 'helpful') {
+      console.log('helpful target matched');
+    }
   }
 
   render() {
