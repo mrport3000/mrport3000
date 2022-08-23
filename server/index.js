9b9c27774317778a79a8178fd252067e2e67c6fe
Duke Romkey <dukeromkey@gmail.com>
2022-08-22 22:10:53 -0500

    Refactored server headers

--- server/index.js
+++ server/index.js
@@ -5,45 +5,34 @@ const axios = require('axios');
 
 const app = express();
 const { PORT } = process.env;
+const headers = {
+  headers: {
+    Authorization: process.env.AUTH,
+  },
+};
 
 app.use(express.static('client/dist'));
 
 app.get('/productinfo/:id', (req, res) => {
-  axios.get(`https://app-hrsei-api.herokuapp.com/api/fec2/hr-rpp/products/${req.params.id}`, {
-    headers: {
-      Authorization: process.env.AUTH,
-    },
-  })
+  axios.get(`https://app-hrsei-api.herokuapp.com/api/fec2/hr-rpp/products/${req.params.id}`, headers)
     .then((result) => res.send(result.data))
     .catch((err) => console.log(err));
 });
 
 app.get('/styles/:id', (req, res) => {
-  axios.get(`https://app-hrsei-api.herokuapp.com/api/fec2/hr-rpp/products/${req.params.id}/styles`, {
-    headers: {
-      Authorization: process.env.AUTH,
-    },
-  })
+  axios.get(`https://app-hrsei-api.herokuapp.com/api/fec2/hr-rpp/products/${req.params.id}/styles`, headers)
     .then((result) => res.send(result.data))
     .catch((err) => console.log(err));
 });
 
 app.get('/related/:id', (req, res) => {
-  axios.get(`https://app-hrsei-api.herokuapp.com/api/fec2/hr-rpp/products/${req.params.id}/related`, {
-    headers: {
-      Authorization: process.env.AUTH,
-    },
-  })
+  axios.get(`https://app-hrsei-api.herokuapp.com/api/fec2/hr-rpp/products/${req.params.id}/related`, headers)
     .then((result) => res.send(result.data))
     .catch((err) => console.log(err));
 });
 
 app.get('/reviews/:id', (req, res) => {
-  axios.get(`https://app-hrsei-api.herokuapp.com/api/fec2/hr-rpp/reviews/meta/?product_id=${req.params.id}`, {
-    headers: {
-      Authorization: process.env.AUTH,
-    },
-  })
+  axios.get(`https://app-hrsei-api.herokuapp.com/api/fec2/hr-rpp/reviews/meta/?product_id=${req.params.id}`, headers)
     .then((result) => res.send(result.data))
     .catch((err) => console.log(err));
 });
