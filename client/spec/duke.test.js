267e4f95fe3b105e7effe4958ee2d48894bc6fe0
Duke Romkey <dukeromkey@gmail.com>
2022-08-25 23:11:59 -0500

    Starting component rendering test

--- client/spec/duke.test.js
+++ client/spec/duke.test.js
@@ -1,5 +1,15 @@
+import { render } from "@testing-library/react";
+import ProductCard from "../src/components/RelatedItems/ProductCard.jsx";
+import '@testing-library/jest-dom';
+
 const sum = require('./sum');
 
 test('adds 1 + 2 to equal 3', () => {
   expect(sum(1, 2)).toBe(3);
+});
+
+describe('Related Products Component', () => {
+  it('Rendered Product Cards', () => {
+    const {} = render(<ProductCard />);
+  });
 });
