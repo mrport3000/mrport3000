f968ebf1c1dd57e21a303d80941ecbcbcf17caac
Keith Hall <j.keithhall@gmail.com>
2022-08-31 22:15:29 -0700

    Keith test suite

---
+++ __mocks__/productInfoMock.js
@@ -0,0 +1,29 @@
+module.exports.productInfo = {
+  "id": 71701,
+  "campus": "hr-rpp",
+  "name": "Heir Force Ones",
+  "slogan": "A sneaker dynasty",
+  "description": "Now where da boxes where I keep mine? You should peep mine, maybe once or twice but never three times. I'm just a sneaker pro, I love Pumas and shell toes, but can't nothin compare to a fresh crispy white pearl",
+  "category": "Kicks",
+  "default_price": "99.00",
+  "created_at": "2022-05-11T19:38:15.373Z",
+  "updated_at": "2022-05-11T19:38:15.373Z",
+  "features": [
+      {
+          "feature": "Sole",
+          "value": "Rubber"
+      },
+      {
+          "feature": "Material",
+          "value": "FullControlSkin"
+      },
+      {
+          "feature": "Mid-Sole",
+          "value": "ControlSupport Arch Bridge"
+      },
+      {
+          "feature": "Stitching",
+          "value": "Double Stitch"
+      }
+  ]
+};
