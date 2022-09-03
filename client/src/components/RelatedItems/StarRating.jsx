7b0555f8b60e0bf6072f38e3936ec5a9b3b9c646
Duke Romkey <dukeromkey@gmail.com>
2022-09-02 21:35:06 -0500

    Added empty star placeholders to StarRating

--- client/src/components/RelatedItems/StarRating.jsx
+++ client/src/components/RelatedItems/StarRating.jsx
@@ -5,11 +5,13 @@ import fullStar from '../../../dist/lib/star-full.png';
 import quarterStar from '../../../dist/lib/star-one-quarter.png';
 import threeQuarterStar from '../../../dist/lib/star-three-quarter.png';
 import emptyStar from '../../../dist/lib/star-outline.png';
+import blankStar from '../../../dist/lib/star-blank.png';
 
 // expect rating to equal avg rating number (example: 4.235)
 function StarRating({ rating }) {
   const avgRating = (Math.floor(rating * 4) / 4).toFixed(2);
   const starStorage = [];
+  const blankStorage = [blankStar, blankStar, blankStar, blankStar, blankStar];
   let i = 1;
 
   for (i; i <= 5; i++) {
@@ -29,6 +31,7 @@ function StarRating({ rating }) {
   return (
     <div>
       {Boolean(rating) && starStorage.map((starFill, index) => <img key={index} className="rating-star" src={starFill} alt="star-rating" />)}
+      {Boolean(!rating) && blankStorage.map((starFill, index) => <img key={index} className="rating-star" src={starFill} alt="star-rating" />)}
     </div>
   );
 }
