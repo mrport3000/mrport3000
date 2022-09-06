4e7e076b3c7b638e784907e82f446203c0345c1c
Eric <e.kalin12@gmail.com>
2022-09-06 19:39:17 -0400

    refactored ReviewModal form data to be more cohesive and properly connect radio buttons to one singular form for required attribute to work correctly

--- client/src/components/RatingsAndReviews/ReviewModal.jsx
+++ client/src/components/RatingsAndReviews/ReviewModal.jsx
@@ -25,6 +25,7 @@ export default class ReviewModal extends React.Component {
 
     };
     this.onChangeValue = this.onChangeValue.bind(this);
+    this.onCharChange = this.onCharChange.bind(this);
     this.handleSubmit = this.handleSubmit.bind(this);
     this.liftStarRating = this.liftStarRating.bind(this);
     this.uploadPhotoHandler = this.uploadPhotoHandler.bind(this);
@@ -37,7 +38,14 @@ export default class ReviewModal extends React.Component {
   }
 
   onChangeValue(e) {
+    // console.log('on change name: ', e.target.name)
     this.setState({ [e.target.name]: e.target.value });
+  }
+
+  onCharChange(e) {
+    const { name } = e.target;
+    const { id } = this.props.characteristics[name];
+    console.log('char id: ', id);
   }
 
   liftStarRating(rating) {
@@ -61,7 +69,7 @@ export default class ReviewModal extends React.Component {
   }
 
   render() {
-    // console.log('Modal Props: ', this.props.characteristics);
+    console.log('Modal Props: ', this.props.characteristics);
     const { characteristics } = this.props;
     const { imgFiles } = this.state;
     if (!this.props.show) {
@@ -72,192 +80,70 @@ export default class ReviewModal extends React.Component {
       <div className="eric-RR-modal">
 
         <div className="eric-RR-modalContainer">
-          <div className="eric-RR-modalHeader">
-            <h2>
-              Add a Review
-            </h2>
-            <h4>
-              About the
-              {' '}
-              {this.props.productName}
-            </h4>
-          </div>
-          <div className="eric-RR-modalRating">
-            <ModalStarRating liftRating={this.liftStarRating} />
-          </div>
-          <div className="eric-RR-recommendProduct">
-            <form>
+          <form onSubmit={this.handleSubmit}>
+            <div className="eric-RR-modalHeader">
+              <h2>
+                Add a Review
+              </h2>
+              <h4>
+                About the
+                {' '}
+                {this.props.productName}
+              </h4>
+            </div>
+            <div className="eric-RR-modalRating">
+              <ModalStarRating liftRating={this.liftStarRating} />
+            </div>
+            <div className="eric-RR-recommendProduct">
 
-              <fieldset>
-                <legend>
-                  Do you recommend this product?
-                </legend>
-                <div>
-                  <input type="radio" value="true" name="recommended" onChange={this.onChangeValue} required="required" />
-                  {' '}
-                  Yes
-                  <input type="radio" value="false" name="recommended" onChange={this.onChangeValue} />
-                  {' '}
-                  No
-                </div>
-              </fieldset>
-
-            </form>
-          </div>
-          <div className="eric-RR-characteristics">
-            <fieldset>
-              <legend>
-                Product Characteristics
-              </legend>
-              {/* <div className="eric-RR-cRating">
-                <div className="eric-RR-r0" />
-                <div className="eric-RR-r1">1</div>
-                <div className="eric-RR-r2">2</div>
-                <div className="eric-RR-r3">3</div>
-                <div className="eric-RR-r4">4</div>
-                <div className="eric-RR-r5">5</div>
-              </div>
-              <div className="eric-RR-cSize">
-                <div className="eric-RR-cTitle"> Size: </div>
-                <div onChange={this.onChangeValue}>
-                  <input type="radio" value="1" name="size" />
-                  {' '}
-                  A Size Too Small
-                  <input type="radio" value="2" name="size" />
-                  {' '}
-                  1/2 Size Too Small
-                  <input type="radio" value="3" name="size" />
-                  {' '}
-                  Perfect
-                  <input type="radio" value="4" name="size" />
-                  {' '}
-                  1/2 Size Too Big
-                  <input type="radio" value="5" name="size" />
-                  {' '}
-                  A Size Too Big
-                </div>
-              </div>
-              <div className="eric-RR-cWidth">
-                <div className="eric-RR-cTitle"> Width: </div>
-                <div onChange={this.onChangeValue}>
-                  <input type="radio" value="1" name="width" />
-                  {' '}
-                  Too Narrow
-                  <input type="radio" value="2" name="width" />
-                  {' '}
-                  Slightly Narrow
-                  <input type="radio" value="3" name="width" />
-                  {' '}
-                  Perfect
-                  <input type="radio" value="4" name="width" />
-                  {' '}
-                  Slightly Wide
-                  <input type="radio" value="5" name="width" />
-                  {' '}
-                  Too Wide
-                </div>
-              </div>
-              <div className="eric-RR-cComfort">
-                <div className="eric-RR-cTitle"> Comfort: </div>
-                <div onChange={this.onChangeValue}>
-                  <input type="radio" value="1" name="comfort" />
-                  {' '}
-                  Uncomfortable
-                  <input type="radio" value="2" name="comfort" />
-                  {' '}
-                  Slightly Uncomfortable
-                  <input type="radio" value="3" name="comfort" />
-                  {' '}
-                  Ok
-                  <input type="radio" value="4" name="comfort" />
-                  {' '}
-                  Comfortable
-                  <input type="radio" value="5" name="comfort" />
-                  {' '}
-                  Perfect
-                </div>
-              </div>
-              <div className="eric-RR-cQuality">
-                <div className="eric-RR-cTitle"> Quality: </div>
-                <div onChange={this.onChangeValue}>
-                  <input type="radio" value="1" name="quality" />
-                  {' '}
-                  Poor
-                  <input type="radio" value="2" name="quality" />
-                  {' '}
-                  Below Average
-                  <input type="radio" value="3" name="quality" />
-                  {' '}
-                  What I Expected
-                  <input type="radio" value="4" name="quality" />
-                  {' '}
-                  Pretty Great
-                  <input type="radio" value="5" name="quality" />
-                  {' '}
-                  Perfect
-                </div>
-              </div>
-              <div className="eric-RR-cLength">
-                <div className="eric-RR-cTitle">Length:</div>
-                <div onChange={this.onChangeValue}>
-                  <input type="radio" value="1" name="length" />
-                  {' '}
-                  Runs Short
-                  <input type="radio" value="2" name="length" />
-                  {' '}
-                  Runs Slightly Short
-                  <input type="radio" value="3" name="length" />
-                  {' '}
-                  Perfect
-                  <input type="radio" value="4" name="length" />
-                  {' '}
-                  Runs Slightly Long
-                  <input type="radio" value="5" name="length" />
-                  {' '}
-                  Runs Long
-                </div>
-              </div>
-              <div className="eric-RR-cFit">
-                <div className="eric-RR-cTitle"> Fit: </div>
-                <div onChange={this.onChangeValue}>
-                  <input type="radio" value="1" name="fit" />
-                  {' '}
-                  Runs Tight
-                  <input type="radio" value="2" name="fit" />
-                  {' '}
-                  Runs Slightly Tight
-                  <input type="radio" value="3" name="fit" />
-                  {' '}
-                  Perfect
-                  <input type="radio" value="4" name="fit" />
-                  {' '}
-                  Runs Slightly Long
-                  <input type="radio" value="5" name="fit" />
-                  {' '}
-                  Runs Long
-                </div>
-              </div> */}
               <div>
+                Do you recommend this product?
+              </div>
+              <div>
+                <input type="radio" value="true" name="recommended" onChange={this.onChangeValue} required />
+                {' '}
+                Yes
+                <input type="radio" value="false" name="recommended" onChange={this.onChangeValue} />
+                {' '}
+                No
+              </div>
+
+            </div>
+            <div className="eric-RR-characteristics">
+
+              <div className="eric-RR-cMainTitle">
+                Product Characteristics
+              </div>
+              <div>
+                <div className="eric-RR-cRating">
+                  <div className="eric-RR-r0" />
+                  <div className="eric-RR-r1">1</div>
+                  <div className="eric-RR-r2">2</div>
+                  <div className="eric-RR-r3">3</div>
+                  <div className="eric-RR-r4">4</div>
+                  <div className="eric-RR-r5">5</div>
+                </div>
                 {
                Object.keys(characteristics).map((char) => {
                  if (char === 'Size') {
+                   //  console.log('Size Id: ', this.props.characteristics['Size'].id)
                    return (
                      <div className="eric-RR-cSize">
                        <div className="eric-RR-cTitle"> Size: </div>
                        <div onChange={this.onChangeValue}>
-                         <input type="radio" value="1" name="size" />
+                         <input type="radio" value="1" name="Size" />
                          {' '}
                          A Size Too Small
-                         <input type="radio" value="2" name="size" />
+                         <input type="radio" value="2" name="Size" />
                          {' '}
                          1/2 Size Too Small
-                         <input type="radio" value="3" name="size" />
+                         <input type="radio" value="3" name="Size" />
                          {' '}
                          Perfect
-                         <input type="radio" value="4" name="size" />
+                         <input type="radio" value="4" name="Size" />
                          {' '}
                          1/2 Size Too Big
-                         <input type="radio" value="5" name="size" />
+                         <input type="radio" value="5" name="Size" />
                          {' '}
                          A Size Too Big
                        </div>
@@ -385,58 +271,47 @@ export default class ReviewModal extends React.Component {
               }
               </div>
 
-            </fieldset>
-          </div>
-          <div className="eric-RR-reviewContainer">
-            <div className="eric-RR-reviewSummary">
-              <form>
+            </div>
+            <div className="eric-RR-reviewContainer">
+              <div className="eric-RR-reviewSummary">
                 <label>
                   <p>Add a review title:</p>
                   <input type="text" name="reviewSummary" maxLength="60" placeholder="Example: Best purchase ever!" value={this.state.value} onChange={this.onChangeValue} />
                 </label>
-              </form>
-            </div>
-            <div className="eric-RR-reviewBody">
-              <form>
+              </div>
+              <div className="eric-RR-reviewBody">
                 <label>
                   <p>Add a written review:</p>
                   <input type="text" name="reviewBody" maxLength="1000" placeholder="Why did you like the product or not?" value={this.state.value} onChange={this.onChangeValue} />
                 </label>
-              </form>
+              </div>
             </div>
-          </div>
-          <div className="eric-RR-uploadPhoto">
-            Upload photos:
-            <input type="file" onChange={this.uploadPhotoHandler} disabled={imgFiles.length >= 5} />
-            <div className="eric-RR-upMap">
-              {
+            <div className="eric-RR-uploadPhoto">
+              Upload photos:
+              <input type="file" onChange={this.uploadPhotoHandler} disabled={imgFiles.length >= 5} />
+              <div className="eric-RR-upMap">
+                {
                 imgFiles.map((file) => <Image cloudName="deitkdfiq" publicId={file.secure_url} />)
                 }
-            </div>
+              </div>
 
-          </div>
-          <div className="eric-RR-nickname">
-            <form>
+            </div>
+            <div className="eric-RR-nickname">
               <label>
                 Add a nickname:
                 <input type="text" name="nickname" maxLength="60" placeholder="Example: jackson11!" value={this.state.value} onChange={this.onChangeValue} />
               </label>
-            </form>
-          </div>
-          <div className="eric-RR-email">
-            <form>
+            </div>
+            <div className="eric-RR-email">
               <label>
                 Add an email address:
                 <input type="text" name="email" maxLength="60" placeholder="Example: jackson11@email.com" value={this.state.value} onChange={this.onChangeValue} />
               </label>
-            </form>
-          </div>
-          <div className="eric-RR-modalSubmit">
-            <form onSubmit={this.handleSubmit}>
+            </div>
+            <div className="eric-RR-modalSubmit">
               <button type="submit">Submit</button>
-            </form>
-
-          </div>
+            </div>
+          </form>
         </div>
       </div>
     );
