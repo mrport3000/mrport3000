f2429f78c9b1e057d864dd6899971da6e1baeb81
Eric <e.kalin12@gmail.com>
2022-09-08 13:11:04 -0400

    successfully reformatted and rendered product breakdown feature without the inverted css icon

--- client/src/components/RatingsAndReviews/ProductBreakdown.jsx
+++ client/src/components/RatingsAndReviews/ProductBreakdown.jsx
@@ -9,22 +9,22 @@ function ProductBreakdown(props) {
   const formatDiv = (key) => {
     const arr = [];
     if (key === 'Size') {
-      arr.push('A size too small', 'Perfect', 'A size too wide');
+      arr.push(key, 'A size too small', 'Perfect', 'A size too wide');
     }
     if (key === 'Width') {
-      arr.push('Too narrow', 'Perfect', 'Too Wide');
+      arr.push(key, 'Too narrow', 'Perfect', 'Too Wide');
     }
     if (key === 'Comfort') {
-      arr.push('Uncomfortable', 'Ok', 'Perfect');
+      arr.push(key, 'Uncomfortable', 'Ok', 'Perfect');
     }
     if (key === 'Quality') {
-      arr.push('Poor', 'What I expected', 'Perfect');
+      arr.push(key, 'Poor', 'What I expected', 'Perfect');
     }
     if (key === 'Length') {
-      arr.push('Runs short', 'Perfect', 'Runs long');
+      arr.push(key, 'Runs short', 'Perfect', 'Runs long');
     }
     if (key === 'Fit') {
-      arr.push('Runs tight', 'Perfect', 'Runs long');
+      arr.push(key, 'Runs tight', 'Perfect', 'Runs long');
     }
     // console.log('props.feature values: ', key);
     // console.log('inner arr: ', arr)
@@ -36,23 +36,26 @@ function ProductBreakdown(props) {
       {
       Object.keys(props.features).map((char) => {
         // console.log('map char: ', char);
-        let charFeatures = formatDiv(char);
+        const charFeatures = formatDiv(char);
 
         return (
           <div className="eric-RR-pbCharacteristic">
-          <div className="eric-RR-pbLeft">
-            <div className="eric-RR-lVisual"></div>
-            <div className="eric-RR-lText">{charFeatures[0]}</div>
+            <div className="eric-RR-pbTitleBreakdown">{charFeatures[0]}</div>
+            <div className="eric-RR-pbScale">
+              <div className="eric-RR-pbLeft">
+                <div className="eric-RR-lVisual"></div>
+                <div className="eric-RR-lText">{charFeatures[1]}</div>
+              </div>
+              <div className="eric-RR-pbCenter">
+                <div className="eric-RR-cVisual"></div>
+                <div className="eric-RR-cText">{charFeatures[2]}</div>
+              </div>
+              <div className="eric-RR-pbRight">
+                <div className="eric-RR-rVisual"></div>
+                <div className="eric-RR-rText">{charFeatures[3]}</div>
+              </div>
+            </div>
           </div>
-          <div className="eric-RR-pbCenter">
-            <div className="eric-RR-cVisual"></div>
-            <div className="eric-RR-cText">{charFeatures[1]}</div>
-          </div>
-          <div className="eric-RR-pbRight">
-            <div className="eric-RR-rVisual"></div>
-            <div className="eric-RR-rText">{charFeatures[2]}</div>
-          </div>
-        </div>
         );
       })
       }
