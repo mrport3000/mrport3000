56e8d579b651bb587e4ccb7429eec68e67174401
Duke Romkey <dukeromkey@gmail.com>
2022-09-11 08:26:44 -0500

    CSS to header change

--- client/src/components/Title.jsx
+++ client/src/components/Title.jsx
@@ -10,7 +10,7 @@ function Title() {
           <h1>Mr. PORT:3000</h1>
         </div>
         <div className="header-right">
-          <h3>Your shortcut to the best deals on the web</h3>
+          <h3>Your shortcut to the <span className="highlight">best deals</span> on the web</h3>
         </div>
       </div>
     </header>
