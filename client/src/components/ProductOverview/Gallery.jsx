9263dfb7236162db2caef654b3bc971332c0ae2d
Keith Hall <j.keithhall@gmail.com>
2022-09-13 16:56:29 -0700

    Update placeholder photo

--- client/src/components/ProductOverview/Gallery.jsx
+++ client/src/components/ProductOverview/Gallery.jsx
@@ -45,7 +45,8 @@ class Gallery extends React.Component {
       handleDownArrowClick,
       zoomed,
     } = this.props;
-    const mainImage = photos[thumbIndex].url || "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTd02zeVLQ2fKKrq6VtQ5fSEvkIGaefaaJTcA&usqp=CAU";
+    const backupPhoto = "https://media.istockphoto.com/photos/coming-soon-neon-sign-the-banner-shining-light-signboard-collection-picture-id1332167985?b=1&k=20&m=1332167985&s=170667a&w=0&h=O-084eNJBhGZGJbJvNvUC1P6d4aSo6XkV4Kom7ZZcIQ=";
+    const mainImage = photos[thumbIndex].url || backupPhoto;
     const img = new Image();
     img.src = mainImage;
     const mainImageWidth = img.naturalWidth;
