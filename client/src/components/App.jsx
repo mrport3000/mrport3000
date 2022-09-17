50fb00fa8c202d7b5643e21b5773b5be96d48424
Keith Hall <j.keithhall@gmail.com>
2022-09-17 08:13:15 -0700

    One final fix

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -276,6 +276,8 @@ class App extends React.Component {
                       recommended: results.data.recommended,
                       outfits,
                       savedToOutfit,
+                      startIndex: 0,
+                      thumbIndex: 0,
                       endIndex,
                     });
                     window.history.pushState({ productId }, '', `?productId=${productId}&styleId=${styleId}`);
@@ -413,6 +415,7 @@ class App extends React.Component {
             handleAddOutfitClick={this.handleAddOutfitClick}
             handleRemoveOutfitClick={this.handleRemoveOutfitClick}
             rating={rating}
+            theme={theme}
           />
         </ErrorBoundary>
         <ErrorBoundary>
